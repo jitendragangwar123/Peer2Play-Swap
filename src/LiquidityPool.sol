b464c11e1a1ab007d1a57b50908cd4438210f3af
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2025-01-17 16:42:37 +0530

    chore:code refactoring

--- src/LiquidityPool.sol
+++ src/LiquidityPool.sol
@@ -1,8 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {ERC20, IERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {ReentrancyMock} from "@openzeppelin/contracts/mocks/ReentrancyMock.sol";
 
