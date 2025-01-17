00839c787552a1830465a03dd59560644c309270
jitendragangwar123 <gangwarjitendra2498@gmail.com>
2025-01-17 20:28:27 +0530

    chore:code refactoring

--- front-end/app/components/Header.tsx
+++ front-end/app/components/Header.tsx
@@ -14,7 +14,7 @@ const Header = () => {
 
   return (
     <>
-      <header className="w-full fixed backdrop-blur-2xl font-arcade font-bold light:border-neutral-800 bg-gradient-to-r from-yellow-600 to-blue-900 lg:light:bg-zinc-600/50 left-0 top-0 z-10 flex flex-wrap gap-4 py-2 px-2 md:py-1 md:px-10 justify-between items-center">
+      <header className="w-full fixed backdrop-blur-2xl font-arcade font-bold light:border-neutral-800 bg-gradient-to-r from-yellow-300 to-red-500 lg:light:bg-zinc-800/50 left-0 top-0 z-10 flex flex-wrap gap-4 py-2 px-2 md:py-1 md:px-10 justify-between items-center">
         <div className="">
           <a href="/">
               <Image
@@ -30,7 +30,55 @@ const Header = () => {
           <div className="flex items-center">
             <w3m-button />
           </div>
+
+          <ThemeSwitch
+            className="flex md:hidden lg:hidden sm:hidden dark:transform-none transform dark:translate-none transition-all duration-500 ease-in-out"
+            action={changeTheme}
+            theme={theme}
+            openMenu={openMenu}
+          />
         </div>
+
+        <div className="flex items-center md:hidden gap-8">
+          <ThemeSwitch
+            className="flex md:hidden dark:transform-none transform dark:translate-none transition-all duration-500 ease-in-out"
+            action={changeTheme}
+            theme={theme}
+            openMenu={openMenu}
+          />
+          <button
+            title="toggle menu"
+            onClick={toggleMenu}
+            className="flex flex-col gap-2 md:hidden"
+          >
+            <div
+              className={`w-[1.5em] h-[2px] ${
+                theme === "dark" ? "bg-[#ffffff]" : "bg-[#000000]"
+              } rounded-full transition-all duration-300 ease-in-out ${
+                openMenu
+                  ? "rotate-45 translate-y-[0.625em]"
+                  : "rotate-0 translate-y-0"
+              }`}
+            ></div>
+            <div
+              className={`w-[1.5em] h-[2px] ${
+                theme === "dark" ? "bg-[#ffffff]" : "bg-[#000000]"
+              } rounded-full transition-all duration-300 ease-in-out ${
+                openMenu ? "opacity-0" : "opacity-100"
+              }`}
+            ></div>
+            <div
+              className={`w-[1.5em] h-[2px] ${
+                theme === "dark" ? "bg-[#ffffff]" : "bg-[#000000]"
+              } rounded-full transition-all duration-300 ease-in-out ${
+                openMenu
+                  ? "-rotate-45 translate-y-[-0.625em]"
+                  : "rotate-0 translate-y-0"
+              }`}
+            ></div>
+          </button>
+        </div>
+
         <div
           className={`w-screen transition-all duration-300 ease-in-out grid ${
             openMenu
